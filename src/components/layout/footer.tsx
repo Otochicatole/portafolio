3f8432274b138e43f921fbe2c510b527026e7b19
Otochicatole <164841448+Otochicatole@users.noreply.github.com>
2025-05-23 19:40:56 -0300

    footer

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -8,7 +8,7 @@ export default function Footer() {
             <div className="relative w-full h-full overflow-hidden">
                 <img
                     loading="eager"
-                    src="/footer.jpg"
+                    src="./footer.jpg"
                     className="w-full max-h-[500px] object-cover object-bottom"
                 />
                 <div className="absolute top-0 w-full h-50 sm:h-100 lg:h-100 bg-gradient-to-b from-[#0e0e0e] via-[#0e0e0ec4] to-transparent"></div>
