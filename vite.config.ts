f27ec328d6271d1cb4e4815a9fff60b8c9466ff2
Otochicatole <164841448+Otochicatole@users.noreply.github.com>
2025-05-23 19:29:57 -0300

    vite

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,5 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(),
     tailwindcss()],
-    base: '/',
+    base: 'https://otochicatole.github.io/portfolio/',
 })
