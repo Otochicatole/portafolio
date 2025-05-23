8756cbe58d359c3d7698ef8758f1ba467e79eb94
Otochicatole <164841448+Otochicatole@users.noreply.github.com>
2025-05-23 20:46:18 -0300

    fix scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import { getRoutes } from './routes/routes';
 import {NavBar} from "./components/layout/nav-bar.tsx";
+import ScrollToTop from './components/layout/scroll-top.tsx';
 
 const App = () => {
     const routes = getRoutes();
@@ -8,6 +9,7 @@ const App = () => {
 
     return (
         <Router>
+            <ScrollToTop />
             <NavBar/>
             <Routes>
                 {routes.map((route) => (
